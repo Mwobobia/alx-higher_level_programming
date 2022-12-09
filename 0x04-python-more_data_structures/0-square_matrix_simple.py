bff0defe7d1e83b89cf0bcc4649715e3bca22011
Mwobobia <Mwobobia>
2022-12-09 19:33:12 +0300

    corrections

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,6 +2,6 @@
 
 
 def square_matrix_simple(matrix=[]):
-    if not matrix or len(matrix) == 0 or matrix[0] == None:
+    if not matrix or len(matrix) == 0:
             return
     return [[i ** 2 for i in j] for j in matrix]
