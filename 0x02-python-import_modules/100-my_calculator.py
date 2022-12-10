ff44c6512f45d1ef634d6739504466c3faa3049f
Mwobobia <Mwobobia>
2022-12-10 17:37:27 +0300

    CORRECTION - a program that imports all functions from the file calculator_1.py and handles basic operations.

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,18 +1,25 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, mul, div
-from sys import argv
 
 if __name__ == "__main__":
-if len(argv) != 4:
-	print("Usage:", argv[0], "<a> <operator> <b>")
-exit(1)
+    from calculator_1 import add, sub, mul, div
+    from sys import argv, exit
 
-	op = argv[2]
-	f = {"+": add, "-": sub, "*": mul, "/": div}
-	if op not in f:
-	print("Unknown operator. Available operators: +, -, * and /")
-exit(1)
+    if len(argv) - 1 != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
 
-	a = int(argv[1])
-b = int(argv[3])
-	print("{:d} {:s} {:d} = {:d}".format(a, op, b, f[op](a, b)))
+    operations = {
+        "+": add,
+        "-": sub,
+        "*": mul,
+        "/": div
+    }
+
+    if argv[2] not in list(operations.keys()):
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+
+    # If the validations are Ok, call the function print_format
+    a = int(argv[1])
+    b = int(argv[3])
+    print("{} {} {} = {}".format(a, argv[2], b, operations[argv[2]](a, b)))
