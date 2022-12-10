4bfeb315a0f8008951a4ca3730223e52611d24ac
Mwobobia <Mwobobia>
2022-12-10 17:41:00 +0300

    CORRECTIONS - the Python function def magic_calculation(a, b): that does exactly the same as the following Python bytecode:

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
-import dis
-# if __name__ == "__main__":
-#     pass
-
 def magic_calculation(a, b):
-	from magic_calculation_102 import add, sub
-	if a < b:
-            c = add(a, b)
-	for element in range(98):
-		add(4, 6)
+    from magic_calculation_102 import add, sub
 
-dis.dis(magic_calculation)
+    if a < b:
+        c = add(a, b)
+        for i in range(4, 6):
+            c = add(c, i)
+        return (c)
+
+    else:
+        return(sub(a, b))
