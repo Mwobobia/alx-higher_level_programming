2f33cf7c1cf4a8b857a4a5b281d5d803716b5f77
Mwobobia <Mwobobia>
2023-01-09 14:23:23 +0300

    Write a function def copy_list(l): that returns a copy of a list

--- 0x09-python-everything_is_object/19-main.py
+++ 0x09-python-everything_is_object/19-main.py
@@ -6,8 +6,8 @@ print(my_list)
 
 new_list = copy_list(my_list)
 
-print(my_list)
-print(new_list)
+    print(my_list)
+    print(new_list)
 
-print(new_list == my_list)
-print(new_list is my_list)
+    print(new_list == my_list)
+    print(new_list is my_list)
