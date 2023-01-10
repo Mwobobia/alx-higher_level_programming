802bd3a96e66e51d737c51a1e139fb5a12699843
Mwobobia <Mwobobia>
2023-01-10 16:43:46 +0300

    A class BaseGeometry (based on 5-base_geometry.py).

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -3,6 +3,6 @@
 
 
 class BaseGeometry():
-        """ This is the skeleton for geo class"""
-        def area(self):
-            raise Exception("area() is not implemented")
+    """ This is the skeleton for geo class"""
+    def area(self):
+        raise Exception("area() is not implemented")
