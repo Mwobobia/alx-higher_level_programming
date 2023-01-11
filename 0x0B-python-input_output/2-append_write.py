7ca9ed58acad55fbb53541316c2dadeaed4f5e79
Mwobobia <Mwobobia>
2023-01-11 17:42:15 +0300

    Append to a file

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -7,4 +7,4 @@ def append_write(filename="", text=""):
     """Appends a string to a file
     """ 
     with open(filename, "a", encoding="utf-8") as f:
-    return(f.write(text))
+        return(f.write(text))
