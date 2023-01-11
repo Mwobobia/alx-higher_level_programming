dd3748b2a5ba8eb9ae1728d552d170972fd9fd09
Mwobobia <Mwobobia>
2023-01-11 18:58:59 +0300

    correction

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,19 +1,9 @@
 #!/usr/bin/python3
-"""Contains the load_from_json_file function
-"""
-
+"""load json"""
 import json
 
 
 def load_from_json_file(filename):
-    """
-    Creates an object from a "JSON file"
-
-    Args:
-        filename(file; txt): file to dump object
-    
-    Return:
-        The return. None
-    """
-    with open(filename, "r", encoding="utf-8") as f:
-        return (json.load(f))
+    """load json"""
+    with open(filename, encoding="utf-8") as f:
+        return json.load(f)
