b6ec24e25f19fdcff77697c9009930a3648ed795
Mwobobia <Mwobobia>
2023-01-11 19:05:47 +0300

    correction

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,30 +1,22 @@
 #!/usr/bin/python3
-"""Contains the Student class
-"""
+"""json"""
 
 
 class Student:
-    """Defines a student
-    """
+    """A student."""
+
     def __init__(self, first_name, last_name, age):
-        """Instanciates student attributes
-        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """retrieves a dictionary
-        representation of a Student
-        instance
-        """
-        my_dict = {}
-        if attrs is None:
+        """Retrieve a dictionary representation of a Student instance."""
+        if attrs is not None and all(isinstance(x, str) for x in attrs):
+            d = {}
+            for k, v in self.__dict__.items():
+                if k in attrs:
+                    d[k] = v
+            return d
+        else:
             return self.__dict__
-
-        for attr in attrs:
-            try:
-                my_dict[attr] = self.__dict__[attr]
-            except KeyError:
-                pass
-        return 
