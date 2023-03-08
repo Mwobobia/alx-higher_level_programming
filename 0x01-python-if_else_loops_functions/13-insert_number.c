d4546bb0fe728f53ed4c144974b38856583be6cb
Mwobobia <Mwobobia>
2023-03-08 16:09:05 +0300

    change

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -9,6 +9,36 @@ listint_t *insert_node(listint_t **head, int number)
 {
     listint_t *new_node, *current;
 
+    /**
+     * main - create list and test insert at beginning of list
+     *
+     * Return: Always 0.
+     */
+    int main(void)
+    {
+        listint_t *head;
+
+        head = NULL;
+        add_nodeint_end(&head, 0);
+        add_nodeint_end(&head, 1);
+        add_nodeint_end(&head, 2);
+        add_nodeint_end(&head, 3);
+        add_nodeint_end(&head, 4);
+        add_nodeint_end(&head, 98);
+        add_nodeint_end(&head, 402);
+        add_nodeint_end(&head, 1024);
+        print_listint(head);
+
+        printf("-----------------\n");
+
+        insert_node(&head, -7);
+
+        print_listint(head);
+
+        free_listint(head);
+
+        return (0);
+    }
     /* Allocate memory for the new node */
     new_node = malloc(sizeof(listint_t));
     if (!new_node)
@@ -24,6 +54,42 @@ listint_t *insert_node(listint_t **head, int number)
         return new_node;
     }
 
+
+    /**
+     * main - create list and test insert multiple numbers
+     *
+     * Return: Always 0.
+     */
+    int main(void)
+    {
+        listint_t *head;
+
+        head = NULL;
+        add_nodeint_end(&head, 0);
+        add_nodeint_end(&head, 1);
+        add_nodeint_end(&head, 2);
+        add_nodeint_end(&head, 3);
+        add_nodeint_end(&head, 4);
+        add_nodeint_end(&head, 98);
+        add_nodeint_end(&head, 402);
+        add_nodeint_end(&head, 1024);
+        print_listint(head);
+
+        printf("-----------------\n");
+
+        insert_node(&head, 5);
+        insert_node(&head, -32);
+        insert_node(&head, 5432);
+        insert_node(&head, 101);
+        insert_node(&head, 47);
+        insert_node(&head, 6405);
+
+        print_listint(head);
+
+        free_listint(head);
+
+        return (0);
+    }
     /* Find the right place to insert the new node */
     current = *head;
     while (current->next && current->next->n < number) {
