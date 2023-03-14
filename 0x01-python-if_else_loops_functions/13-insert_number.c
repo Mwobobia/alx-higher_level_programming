9bf66adfb768f5fbe8900e5e33ec6bc6775fec4e
Mwobobia <Mwobobia>
2023-03-14 17:24:56 +0300

    linkedlist.c

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,39 +1,15 @@
-#include <stdlib.h>
-
-typedef struct listint_s {
-    int n;
-    struct listint_s *next;
-} listint_t;
-
+#include "lists.h"
+/**
+ * insert_node - inserts a number into a sorted singly linked list
+ * @head: head
+ * @number: num
+ *
+ * Return: address of the new node, or NULL if it failed
+ */
 listint_t *insert_node(listint_t **head, int number)
 {
-    listint_t *new_node, *current;
+(void) number;
+(void) head;
 
-    /* Allocate memory for the new node */
-    new_node = malloc(sizeof(listint_t));
-    if (!new_node)
-        return NULL;
-
-    /* Fill the new node with the provided number */
-    new_node->n = number;
-    new_node->next = NULL;
-
-    /* If the list is empty, insert the new node as the head */
-    if (*head == NULL) {
-        *head = new_node;
-        return new_node;
-    }
-
-    /* Find the right place to insert the new node */
-    current = *head;
-    while (current->next && current->next->n < number) {
-        current = current->next;
-    }
-
-    /* Insert the new node into the list */
-    new_node->next = current->next;
-    current->next = new_node;
-
-    return new_node;
+return (0);
 }
-
