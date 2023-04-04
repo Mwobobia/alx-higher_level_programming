e80fad591533304be94c06647481c49c202e3ffe
Mwobobia <Mwobobia>
2023-04-04 16:07:00 +0300

    String representation - a class Rectangle that defines a rectangle by: (based on 2-rectangle.py)

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -6,61 +6,62 @@ empty class Rectangle
 
 
 class Rectangle:
-	"""
-	Rectangle empty class
-	"""
-	def __init__(self, width=0, height=0):
-		if type(width) is not int:
-			raise TypeError("width must be an integer")
-		if type(height) is not int:
-			raise TypeError("height must be an integer")
-		if width < 0:
-			raise ValueError("width must be >= 0")
-		if height < 0:
-			raise ValueError("height must be >= 0")
-		self.__height = height
-		self.__width = width
+    """
+    Rectangle empty class
+    """
 
-	@property
-	def width(self):
-		return self.__width
 
-	@property
-	def height(self):
-		return self.__height
+def __init__(self, width=0, height=0):
+    if type(width) is not int:
+        raise TypeError("width must be an integer")
+    if type(height) is not int:
+        raise TypeError("height must be an integer")
+    if width < 0:
+        raise ValueError("width must be >= 0")
+    if height < 0:
+        raise ValueError("height must be >= 0")
+    self.__height = height
+    self.__width = width
 
-	@height.setter
-	def height(self, value):
-		if type(value) is not int:
-			raise TypeError("height must be an integer")
-		if value < 0:
-			raise ValueError("height must be >= 0")
-		self.__height = value
+    @property
+    def width(self):
+        return self.__width
 
-	@width.setter
-	def width(self, value):
-		if type(value) is not int:
-			raise TypeError("width must be an integer")
-		if value < 0:
-			raise ValueError("width must be >= 0")
-		self.__width = value
+    @property
+    def height(self):
+        return self.__height
 
-	def area(self):
-		return self.__width * self.__height
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
 
-	def perimeter(self):
-		if self.__width == 0 or self.__height == 0:
-			return 0
-		return self.__width * 2 + self.__height * 2
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
 
-	def __str__(self):
-		string = ""
-		if self.__width == 0 or self.__height == 0:
-			return string
-		for item in range(self.__height):
-			if item != self.__height - 1:
-				string = string + "#" * self.__width + "\n"
-			else:
-				string = string + "#" * self.__width
-		return string
+    def area(self):
+        return self.__width * self.__height
 
+    def perimeter(self):
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return self.__width * 2 + self.__height * 2
+
+    def __str__(self):
+        string = ""
+        if self.__width == 0 or self.__height == 0:
+            return string
+        for item in range(self.__height):
+            if item != self.__height - 1:
+                string = string + "#" * self.__width + "\n"
+            else:
+                string = string + "#" * self.__width
+        return string
