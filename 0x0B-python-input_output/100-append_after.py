c3adb04b9fb9ac3b93c6563571ae3b4282372628
Mwobobia <Mwobobia>
2023-04-14 18:36:18 +0300

    Reach חי completed tasks - Complete the 9 next tasks, getting to 18 total.

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,23 +1,24 @@
 #!/usr/bin/python3
 def append_after(filename="", search_string="", new_string=""):
 
+    """
+
+Inserts a line of text to a file, after each line containing a specific
+/string.
+
+Args:
+filename(str): The name of the file.
+search_string(str): The string to search for.
+new_string(str): The new string to append.
 
     """
-    Inserts a line of text to a file, after each line containing a specific 
-    /string.
 
-    Args:
-        filename (str): The name of the file.
-        search_string (str): The string to search for.
-        new_string (str): The new string to append.
 
-    """
-    with open(filename, mode='r+', encoding='utf-8') as f:
-        lines = f.readlines()
-        f.seek(0)
-        for line in lines:
-            f.write(line)
-            if search_string in line:
-                f.write(new_string)
+with open(filename, mode='r+', encoding='utf-8') as f:
+    lines = f.readlines()
+    f.seek(0)
+    for line in lines:
+        f.write(line)
+        if search_string in line:
+            f.write(new_string)
         f.truncate()
-
