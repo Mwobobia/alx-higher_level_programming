1b54430cfdf85dfb9506025827089cc3f711f7f0
root <Mwobobia>
2023-05-23 04:46:10 -0700

    Go to UTF8

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,11 @@
+-- Converts hbtn_0c_0 database to UTF8 (utf8mb4, collate utf8mb4_unicode_ci) my your MySQL server
+
+-- Convert all of the following to UTF8:
+
+--     Database hbtn_0c_0
+--     Table first_table
+--     Field name in first_table
+
+USE hbtn_0c_0
+ALTER TABLE first_table
+CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
